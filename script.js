46d92f8da926d8a91e22a4cf397a99528aceae8e
Annie Huang <annie.hn@gmail.com>
2023-08-12 17:12:28 +1000

    List addNodes and removeNodes

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/*
+Each MutationRecord has the following attributes {
+  addedNodes: [div]           << linked to 'childList' target
+  attributeName
+  attributeNamespace
+  nextSibling
+  oldValue
+  previousSibling
+  removedNodes: [div.child]   << linked to 'childList' target
+  target: div#parent.parent
+  type: "childList"
+}
+* */
+
 const mutationObserver = new MutationObserver((entries) => {
   console.log(entries);
 });
@@ -5,3 +19,7 @@ const mutationObserver = new MutationObserver((entries) => {
 const parent = document.querySelector('.parent');
 
 mutationObserver.observe(parent, { childList: true });
+// mutationObserver.observe(parent.children[0], { childList: true });
+
+parent.children[0].remove();
+parent.appendChild(document.createElement('div'));
