b3fd8938ad00db9c42d689b0fe8be697db9f4c41
Annie Huang <annie.hn@gmail.com>
2023-08-12 17:25:11 +1000

    MutationObserver will observe every event loop

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ Each MutationRecord has the following attributes {
 }
 * */
 
+// Every time the document of our screen changes its content, our mutation observer is going to take all the changes
+// that occurred and put them inside this entries array for us. So the array can be large or small depending on how many changes we made
 const mutationObserver = new MutationObserver((entries) => {
   console.log(entries);
 });
@@ -22,4 +24,12 @@ mutationObserver.observe(parent, { childList: true });
 // mutationObserver.observe(parent.children[0], { childList: true });
 
 parent.children[0].remove();
-parent.appendChild(document.createElement('div'));
+
+// Every single time you make changes to your dom, it's going to take all the changes that happened between the last time they were rendered to teh screen and
+// this current time and they are going to put them all inside of one array
+// parent.appendChild(document.createElement('div'));
+
+// If you put this into a timeout, you will notice it print out 2 arrays with 1 element each (rather than 1 array of 2 elements)
+setTimeout(() => {
+  parent.appendChild(document.createElement('div'));
+}, 100);
